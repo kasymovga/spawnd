dbd641ffbd2c7bf84bd1867e4b35f6c53aa403b1
Grigorii Kasymov <kasymovga@xsmail.com>
2014-11-18 12:11:21 +0500

    Little cleaning

--- logwtmp.c
+++ logwtmp.c
@@ -9,18 +9,14 @@ void usage() {
 };
 
 int main(int argc, char **argv) {
-	cmd=argc?(argv[0]?argv[0]:""):"";
-	const char *line, *name, *host;
-	int ret=EXIT_FAILURE;
-	if(argc!=4) {
+	cmd = argc ? (argv[0] ? argv[0] : "") : "";
+	int ret = EXIT_FAILURE;
+	if(argc != 4) {
 		usage();
 		goto finish;
 	}
-	line=argv[1];
-	name=argv[2];
-	host=argv[3];
 	logwtmp(argv[1], argv[2], argv[3]);
-	ret=EXIT_SUCCESS;
+	ret = EXIT_SUCCESS;
 finish:
 	return ret;
 };
